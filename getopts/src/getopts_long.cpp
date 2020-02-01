5306b19ab32262093e0bf87a4de5906b81ea5c10
Dmitry Fedotov <dm.fe@yandex.ru>
2020-02-01 03:20:46 +0300

    Added the getopts sample.

---
+++ getopts/src/getopts_long.cpp
@@ -0,0 +1,72 @@
+/*-----------------------------------------------------------------------------
+* File Name: getopts_long.cpp
+* Purpose:
+* Creation Date: 01-02-2020
+* Last Modified: Sat 01 Feb 2020 03:15:11 AM MSK
+* Created by: dima
+-----------------------------------------------------------------------------*/
+
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* program_name;
+
+void print_usage (FILE* stream, int exit_code) {
+    fprintf (stream, "Usage: %s options [ inputfile ... ]\n", program_name);
+    fprintf (stream, "  -h  --help             Displays this usage information.\n"
+                     "  -o  --output filename  Writes output to file.\n"
+                     "  -v  --verbose          Prints verbose messages.\n");
+
+    exit (exit_code);
+}
+
+int main (int argc, char* argv[]) {
+
+    int next_option;
+
+    const char* const short_options = "ho:v";
+    const struct option long_options[] = {
+        { "help",    0, NULL, 'h' },
+        { "output",  1, NULL, 'o' },
+        { "verbose", 0, NULL, 'v' },
+        { NULL,      0, NULL, 0}
+    };
+
+    const char* output_filename = NULL;
+    int verbose = 0;
+
+    program_name = argv[0];
+
+    do {
+        next_option = getopt_long(argc, argv, short_options,
+                                  long_options, NULL);
+
+        switch (next_option) {
+            case 'h':
+                print_usage (stdout, 0);
+            case 'o':
+                output_filename = optarg;
+                break;
+            case 'v':
+                verbose = 1;
+                break;
+            case '?':
+                print_usage(stderr, 1);
+            case -1:
+                break;
+            default:
+                abort();
+        }
+    } while (next_option != -1);
+
+    if (verbose) {
+        for (int i = 1; i < argc; i++) {
+            printf ("Argument: %s\n", argv[i]);
+        }
+    }
+
+    /* Here should be the rest of the programm. */
+
+    return 0;
+}
