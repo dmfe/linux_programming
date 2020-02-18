94ee4bf8209d372dbe7d23947bc3036a117f8409
Dmitry Fedotov <dm.fe@yandex.ru>
2020-02-19 02:06:59 +0300

    Playing around with ncurses.

--- ncurses_hello/src/hello.cpp
+++ ncurses_hello/src/hello.cpp
@@ -2,17 +2,33 @@
 * File Name: hello.cpp
 * Purpose:
 * Creation Date: 13-02-2020
-* Last Modified: Thu 13 Feb 2020 10:50:16 PM MSK
+* Last Modified: Wed 19 Feb 2020 01:45:05 AM MSK
 * Created by: dima
 -----------------------------------------------------------------------------*/
 
 #include <ncurses.h>
 
 int main () {
+
     initscr();
-    printw("Hello world!\n");
+    cbreak();
+    noecho();
+
+    int height, width, start_y, start_x;
+    height = 10;
+    width = 20;
+    start_y = start_x = 10;
+
+    WINDOW *win = newwin(height, width, start_y, start_x);
     refresh();
+
+    box(win, 0, 0);
+    mvwprintw(win, 1, 1, "This is my box");
+    wrefresh(win);
+
     getch();
+    getch();
+
     endwin();
 
     return 0;
