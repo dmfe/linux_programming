fe40e9528927b060747889b66b2930a3d09e8b0a
Dmitry Fedotov <dm.fe@yandex.ru>
2020-02-23 01:57:53 +0300

    Playing around with ncurses. Part 2.

--- ncurses_hello/src/hello.cpp
+++ ncurses_hello/src/hello.cpp
@@ -2,11 +2,14 @@
 * File Name: hello.cpp
 * Purpose:
 * Creation Date: 13-02-2020
-* Last Modified: Wed 19 Feb 2020 01:45:05 AM MSK
+* Last Modified: Sun 23 Feb 2020 01:24:08 AM MSK
 * Created by: dima
 -----------------------------------------------------------------------------*/
 
 #include <ncurses.h>
+#include <string>
+
+using namespace std;
 
 int main () {
 
@@ -14,21 +17,47 @@ int main () {
     cbreak();
     noecho();
 
-    int height, width, start_y, start_x;
-    height = 10;
-    width = 20;
-    start_y = start_x = 10;
+    int y_max, x_max;
+    getmaxyx(stdscr, y_max, x_max);
 
-    WINDOW *win = newwin(height, width, start_y, start_x);
+    WINDOW *menu_win = newwin(6, x_max - 12, y_max - 8, 5);
+    box(menu_win, 0, 0);
     refresh();
+    wrefresh(menu_win);
+    keypad(menu_win, true);
 
-    box(win, 0, 0);
-    mvwprintw(win, 1, 1, "This is my box");
-    wrefresh(win);
+    string choices[3] = {"Walk", "Jog", "Run"};
+    int choice;
+    int highlight = 0;
+
+    while (1) {
+        for (int i = 0; i < 3; i++) {
+            if (i == highlight)
+                wattron(menu_win, A_REVERSE);
+            mvwprintw (menu_win, i + 1, 1, choices[i].c_str());
+            wattroff(menu_win, A_REVERSE);
+        }
+        choice = wgetch (menu_win);
+
+        switch (choice) {
+            case KEY_UP:
+                highlight--;
+                break;
+            case KEY_DOWN:
+                highlight++;
+                break;
+            default:
+                break;
+        }
+
+        if (choice == 10) {
+            break;
+        }
+    }
+
+    printw ("Your choice was: %s", choices[highlight].c_str());
 
     getch();
-    getch();
-
     endwin();
 
     return 0;
