c04c0c5cebd981c5964fc5f6d636a220815c29b1
Dmitry Fedotov <dm.fe@yandex.ru>
2020-06-06 15:45:19 +0300

    Waiting for the child process example.

--- create-process/src/main.cpp
+++ create-process/src/main.cpp
@@ -2,11 +2,12 @@
 * File Name: main.cpp
 * Purpose:
 * Creation Date: 06-06-2020
-* Last Modified: Sat 06 Jun 2020 11:52:45 AM MSK
+* Last Modified: Sat 06 Jun 2020 03:43:58 PM MSK
 * Created by: dima
 -----------------------------------------------------------------------------*/
 
 #include <stdio.h>
+#include <sys/wait.h>
 #include "process.h"
 
 int main(int argc, char* argv[]) {
@@ -18,10 +19,18 @@ int main(int argc, char* argv[]) {
         NULL
     };
 
-    create_process_system();
+    //create_process_system();
     create_process_fork_exec("ls", arg_list);
 
-    printf("done with main program\n");
+    int child_state;
+    wait(&child_state);
+    if (WIFEXITED(child_state)) {
+        printf("The child process exited normally, with exit code:"
+                " %d\n", WEXITSTATUS(child_state));
+    } else {
+        printf("The child process exited abnormally\n");
+    }
 
+    printf("done with main program\n");
     return 0;
 }
